a70c5bbcfd156df66675ea7626e9b345bbc52154
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-27 13:13:27 +0100

    ADD: added the object array inside data in the js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
 const { createApp } = Vue
 
-createApp({
-  data() {
-    return {
-      
-        
-    }
-  }
-}).mount('#app')
+  createApp({
+    data() {
+      return {
+        todos: [
+            {
+                text: "Fare la spesa",
+                done: false,
+            },
+            {
+                text: "Fare i compiti",
+                done: false,
+            },
+            {
+                text: "Lavare la macchina",
+                done: false,
+            },
+            {
+                text: "Sistemare camera",
+                done: false,
+            },
+            {
+                text: "Cucinare",
+                done: false,
+            },
+          ],
+      }
+    },
+  }).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
